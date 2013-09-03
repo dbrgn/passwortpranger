97cd2a8de8f3c36c3aee3c263b8df4e922e5b5f8
Danilo Bargen <gezuru@gmail.com>
2013-09-03 23:14:24 +0200

    Dynamic site list

--- pranger/front/views.py
+++ pranger/front/views.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function, division, absolute_import, unicode_literals
 
-from django.core.urlresolvers import reverse
-from django.contrib import messages
-from django.contrib.auth import logout
-from django.shortcuts import redirect
-from django.views.generic.base import View, TemplateView
+from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
+
+from front import models
 
 
-class HomeView(TemplateView):
+class HomeView(ListView):
+    model = models.Website
     template_name = 'front/home.html'
