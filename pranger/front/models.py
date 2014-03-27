072345718230cec34c7784843a647a085f187019
Remo Liebi <remo@liebi.net>
2014-03-27 20:55:51 +0100

    added Bewertung Model

--- pranger/front/models.py
+++ pranger/front/models.py
@@ -22,20 +22,26 @@ class Website(models.Model):
         (0, 'Nein'),
         (1, 'Nur Login'),
         (2, 'Überall'),
+        (3, 'Forced')
     )
-
+    PW_MAX_LENGTH_CHOICES = (
+        (0, '< 16'),
+        (1, '<10'),
+        (2, 'unlimited')
+    )
     # Basic properties
     name = models.CharField(max_length=255)
     url = models.URLField(max_length=255)
     description = models.TextField(blank=True)
 
     # Password
-    pw_plaintext = models.NullBooleanField()
-    pw_min_length = models.SmallIntegerField(null=True, blank=True)
-    pw_max_length = models.SmallIntegerField(null=True, blank=True)
-    pw_alphabet_size = models.SmallIntegerField(null=True, blank=True)
-    pw_salted = models.NullBooleanField()
-    pw_hashfunction = models.CharField(max_length=32, null=True, blank=True)
+    pw_max_length = models.SmallIntegerField(choices=PW_MAX_LENGTH_CHOICES)
+    pw_alphabet_size_restricted = models.BooleanField()
+
+    #delivery
+    remember_mail_has_own_password = models.BooleanField()
+    remember_mail_has_new_password = models.BooleanField()
+    new_user_mail_has_own_password = models.BooleanField()
 
     # Email
     eml_reg_plaintext = models.NullBooleanField(
@@ -46,10 +52,35 @@ class Website(models.Model):
     # Other security measures
     tls = models.SmallIntegerField(choices=TLS_CHOICES)
     twofactor = models.NullBooleanField()
-
+    securitywidget = models.BooleanField()
     def get_canonical_slug(self):
         """Canonical slug for the URL."""
         return slugify(self.name)
 
     def __unicode__(self):
         return self.name
+
+
+class Bewertung():
+    BASE_POINTS = 3
+    PW_MAX_LENGTH = (
+        (0, -1),
+        (1, -2),
+        (2, 1)
+    )
+    ALPHABET_SIZE_RESTRICTED = -1
+    NEW_PASSWORD_IN_REMBMER_MAIL = -1
+    OWN_PASSWORD_IN_REMBMER_MAIL = -3
+    OWN_PASSWORD_IN_REGISTER_MAIL = -1
+    TLS_SCORE = (
+        (0, -2),
+        (1, -1),
+        (2, 0),
+        (3, 1)
+    )
+    TWO_FACTOR = 3
+    SECURITY_WIDGET = 1
+
+    actual_points = 0
+    def __init__(self, website):
+        self.actual_points = self.BASE_POINTS + self.PW_MAX_LENGTH[website.pw_max_length]
