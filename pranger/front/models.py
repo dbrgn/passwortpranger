d138bf2795cc87114719849e7af5defb999a0821
Danilo Bargen <mail@dbrgn.ch>
2014-12-14 17:34:43 +0100

    Refactored site view, moved logic into model

--- pranger/front/models.py
+++ pranger/front/models.py
@@ -1,17 +1,26 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function, division, absolute_import, unicode_literals
 
+from collections import defaultdict, namedtuple
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.text import slugify
+from django.utils.functional import cached_property
+
+
+Rating = namedtuple('Rating', 'positive negative total')
 
 
 class User(AbstractUser):
-    """A custom user model that extends the default user."""
-
+    """
+    A custom user model that extends the default user.
+    """
     def name(self):
-        """Return either full user first and last name or the username, if no
-        further data is found."""
+        """
+        Return either full user first and last name or the username, if no
+        further data is found.
+        """
         if self.first_name or self.last_name:
             return ' '.join(filter(None, [self.first_name, self.last_name]))
         return self.username
@@ -54,8 +63,83 @@ class Website(models.Model):
     pw_strength_indicator = models.NullBooleanField(
         help_text='Does the website offer a visual password strength indicator?')
 
+    @cached_property
+    def scores(self):
+        """
+        Return a dictionary with positive and negative scores.
+
+        This property is cached on the model instance.
+
+        """
+        scores = defaultdict(dict)
+
+        # Password length
+        if self.pw_max_length == 0:
+            scores['negative']['MAX_LEN_16'] = -1
+        elif self.pw_max_length == 1:
+            scores['negative']['MAX_LEN_10'] = -2
+        elif self.pw_max_length == 3:
+            scores['positive']['MAX_LEN_UNLIMITED'] = 1
+
+        # Alphabet
+        if self.alphabet_limited:
+            scores['negative']['ALPHABET_LIMITED'] = -1
+
+        # Plaintext
+        if self.eml_registration_plaintext:
+            scores['negative']['PW_IN_REGISTRATION_MAIL'] = -1
+        if self.eml_recovery_plaintext:
+            scores['negative']['NEW_PW_IN_REMEMBER_MAIL'] = -1
+        if self.eml_reminder_plaintext:
+            scores['negative']['OWN_PW_IN_REMEMBER_MAIL'] = -3
+
+        # Encryption
+        if self.tls == 0:
+            scores['negative']['TLS_NO'] = -2
+        elif self.tls == 1:
+            scores['negative']['TLS_SOME'] = -1
+        elif self.tls == 2:
+            scores['hint']['TLS_ALL'] = 0
+        elif self.tls == 3:
+            scores['positive']['TLS_FORCED'] = 1
+
+        # MFA
+        if self.two_factor:
+            scores['positive']['TWO_FACTOR'] = 3
+
+        # Other
+        if self.pw_strength_indicator:
+            scores['positive']['SECURITY_WIDGET'] = 1
+
+        return scores
+
+    @cached_property
+    def ratings(self):
+        """
+        Return namedtuple with ratings for this site.
+
+        The namedtuple has three keys: ``positive``, ``negative``
+        and ``total``.
+
+        This property is cached on the model instance.
+
+        """
+        scores = self.scores
+        sum_pos = 4 + sum(scores['positive'].itervalues())
+        if sum_pos > 6:
+            sum_pos = 6
+        sum_neg = sum(scores['negative'].itervalues())
+        if sum_neg < -6:
+            sum_neg = -6
+        total = sum_neg + sum_pos
+        if total < 0:
+            total = 0
+        return Rating(sum_pos, sum_neg, total)
+
     def get_canonical_slug(self):
-        """Canonical slug for the URL."""
+        """
+        Canonical slug for the URL.
+        """
         return slugify(self.name)
 
     def __unicode__(self):
--- pranger/front/views.py
+++ pranger/front/views.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function, division, absolute_import, unicode_literals
 
-from collections import defaultdict
-
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic.base import TemplateView
 from django.views.generic.detail import DetailView
@@ -23,64 +21,6 @@ class WebsiteView(CanonicalSlugDetailMixin, DetailView):
     model = models.Website
     template_name = 'front/website.html'
     context_object_name = 'site'
-
-    def get_context_data(self, **kwargs):
-        context = super(WebsiteView, self).get_context_data(**kwargs)
-        website = self.object
-        scores = defaultdict(dict)
-
-        if website.pw_max_length == 0:
-            scores['negative']['MAX_LEN_16'] = -1
-        elif website.pw_max_length == 1:
-            scores['negative']['MAX_LEN_10'] = -2
-        elif website.pw_max_length == 3:
-            scores['positive']['MAX_LEN_UNLIMITED'] = 1
-
-        if website.alphabet_limited:
-            scores['negative']['ALPHABET_LIMITED'] = -1
-
-        if website.eml_registration_plaintext:
-            scores['negative']['PW_IN_REGISTRATION_MAIL'] = -1
-
-        if website.eml_recovery_plaintext:
-            scores['negative']['NEW_PW_IN_REMEMBER_MAIL'] = -1
-
-        if website.eml_reminder_plaintext:
-            scores['negative']['OWN_PW_IN_REMEMBER_MAIL'] = -3
-
-        if website.tls == 0:
-            scores['negative']['TLS_NO'] = -2
-        elif website.tls == 1:
-            scores['negative']['TLS_SOME'] = -1
-        elif website.tls == 2:
-            scores['hint']['TLS_ALL'] = 0
-        elif website.tls == 3:
-            scores['positive']['TLS_FORCED'] = 1
-
-        if website.two_factor:
-            scores['positive']['TWO_FACTOR'] = 3
-
-        if website.pw_strength_indicator:
-            scores['positive']['SECURITY_WIDGET'] = 1
-
-        scores['sum'] = self.calculate(scores['positive'].itervalues(),
-                                       scores['negative'].itervalues())
-
-        context['scores'] = scores
-        return context
-
-    @staticmethod
-    def calculate(positive, negative):
-        sum_pos = 4 + sum(positive)
-        if sum_pos > 6:
-            sum_pos = 6
-        sum_neg = sum(negative)
-        if sum_neg < -6:
-            sum_neg = -6
-        total = sum_neg + sum_pos
-        if total < 0:
-            total = 0
-        return {'positive': sum_pos, 'negative': sum_neg, 'total': total}
 
 
 class InfoView(TemplateView):
