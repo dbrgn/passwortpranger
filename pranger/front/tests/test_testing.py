ecc866adf2f097609a5b2b232d201e0ea11fd784
Danilo Bargen <mail@dbrgn.ch>
2014-12-14 17:39:05 +0100

    Fixed tests

--- pranger/front/tests/test_testing.py
+++ pranger/front/tests/test_testing.py
@@ -3,18 +3,23 @@ from __future__ import print_function, division, absolute_import, unicode_litera
 
 import pytest
 
-from front.models import User
+from django.contrib.auth import get_user_model
 
 
 def test_dummy():
-    """Assert that the Webrepublic is awesome."""
-    webrepublic = 'awesome'
-    assert webrepublic == 'awesome'
+    """
+    Assert that 1 + 1 equals 2.
+    """
+    one = 1
+    assert one + 1 == 2
 
 
 @pytest.mark.django_db
 def test_database():
-    """Check whether database access works."""
+    """
+    Check whether database access works.
+    """
+    User = get_user_model()
     assert User.objects.count() == 0
     User.objects.create(username='dummy', email='dummy@example.com')
     assert User.objects.count() == 1
