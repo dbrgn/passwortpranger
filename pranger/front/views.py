89cd6fac23fb9f94274c375cb4d23bc639226ebc
Danilo Bargen <mail@dbrgn.ch>
2015-01-06 21:56:04 +0100

    Implemented email notifications (fixes #3)

--- pranger/front/views.py
+++ pranger/front/views.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function, division, absolute_import, unicode_literals
 
-from django.core.urlresolvers import reverse_lazy
+from django.conf import settings
+from django.core.urlresolvers import reverse, reverse_lazy
+from django.core.mail import send_mail
 from django.views.generic.base import TemplateView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
@@ -32,6 +34,24 @@ class SubmissionView(CreateView):
     fields = ['url', 'email', 'comment']
     template_name = 'front/submission.html'
     success_url = reverse_lazy('submission_thanks')
+
+    def form_valid(self, form):
+        handler = super(SubmissionView, self).form_valid(form)
+
+        # Send notification email
+        if settings.ADMINS:
+            subject = '[passwortpranger] New Submission: %s' % self.object.url
+            message = 'There is a new submission on Passwortpranger:\n\n%s%s' % (
+                    settings.SITE_URL,
+                    reverse('admin:front_submission_change', args=[self.object.pk])
+            )
+            sender = self.object.email or 'noreply@passwortpranger.ch'
+            receivers = ['%s <%s>' % (admin[0], admin[1]) for admin in settings.ADMINS]
+            send_mail(subject, message, sender, receivers, fail_silently=False)
+        else:
+            print('No admin e-mails found')
+
+        return handler
 
 
 class SubmissionThanksView(TemplateView):
